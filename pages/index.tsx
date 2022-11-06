ef6a72bf5ba85baf06435ff6c30c83ca2a19939e
Isaiah Taylor <isaiah.p.taylor@gmail.com>
2022-11-06 13:33:12 -0800

    added colors and theme mode, SSR for sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ThemeContext } from '../contexts/themeContext'
 
 import { createClient } from "next-sanity";
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const client = createClient({
   projectId: "tyc9omzx",
@@ -21,65 +22,88 @@ async function getPosts() {
 
 function getPreferredColorScheme() {
   if (typeof window !== 'undefined') {
-  if (window.matchMedia) {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      return 'dark';
-    } else {
-      return 'light';
+    if (window.matchMedia) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark';
+      } else {
+        return 'light';
+      }
     }
-  }
   }
   return 'light';
 }
 
-const Home: NextPage = () => {
+const Home: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({posts}) => {
 
-  const [posts, setPosts] = useState<any[]>([]);
-
-  const darkMode = getPreferredColorScheme() === 'dark';
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
 
   useEffect(() => {
-    getPosts().then((posts) => {
-      setPosts(posts);
-    });
+    if (typeof window !== 'undefined') {
+      if (window.matchMedia) {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          setColorMode('dark');
+        } else {
+          setColorMode('light');
+        }
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+          setColorMode(e.matches ? 'dark' : 'light');
+        });
+      }
+    }
   }, []);
 
   return (
-    <div className='bg-white dark:bg-raisin-black'>
-      <Head>
-        <title>Isaiah Taylor</title>
-        <meta name="description" content="Isaiah Taylor's writings" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ThemeContext.Provider value={{
+      colorMode,
+      toggleTheme: () => {
+        setColorMode(colorMode === 'light' ? 'dark' : 'light');
+      }
+    }}>
+      <div className='bg-white dark:bg-raisin-black text-eerie-black dark:text-platinum'>
+        <Head>
+          <title>Isaiah Taylor</title>
+          <meta name="description" content="Isaiah Taylor's writings" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className='flex'>
-        <div className='flex flex-col justify-between grow p-[100px]'>
-          <div className='flex flex-col gap-5'>
-            {posts.map((post) => (
-              <div className='text-eerie-black dark:text-platinum font-display font-bold ' key={post._id}>
-                <h1 className='text-4xl'>{post.title}</h1>
-                {/* <p>{post.body}</p> */}
-              </div>
-            ))}
-          </div>
+        <main className='flex'>
+          <div className='flex flex-col justify-between grow p-[100px]'>
+            <div className='flex flex-col gap-5'>
+              {posts.map((post) => (
+                <div className='font-display font-bold ' key={post._id}>
+                  <h1 className='text-4xl'>{post.title}</h1>
+                  {/* <p>{post.body}</p> */}
+                </div>
+              ))}
+            </div>
 
-          <div className='flex flex-col text-eerie-black dark:text-platinum font-display font-bold text-xl'>
-            <p>Twitter</p>
-            <p>Instagram</p>
-            <p>LinkedIn</p>
+            <div className='flex flex-col font-display font-bold text-xl'>
+              <p>Twitter</p>
+              <p>Instagram</p>
+              <p>LinkedIn</p>
+            </div>
           </div>
-        </div>
-        <div className='flex justify-center items-center align-middle w-[600px] h-screen'>
-          <Image
-            src={darkMode ? "/IT-dark.svg" : "/IT.svg"}
-            alt="Isaiah Taylor"
-            width={350}
-            height={350}
-          />
-        </div>
-      </main>
-    </div>
+          <div className='flex justify-center items-center align-middle w-[600px] h-screen'>
+            <Image
+              src={colorMode === 'dark' ? "/IT-dark.svg" : "/IT.svg"}
+              alt="Isaiah Taylor"
+              width={350}
+              height={350}
+            />
+          </div>
+        </main>
+      </div>
+    </ThemeContext.Provider>
   )
+}
+
+export async function getServerSideProps() {
+  const posts = (await getPosts() as any[]);
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
         'eerie-black': '#141415',
         'platinum': '#E6E6E6',
         'cultured': '#F5F5F5',
+        'baby-powder-white': 'FBFEF9',
+        'star-command-blue': '0E79B2',
+        'maroon-x11': 'BF1363',
+        'deep-saffron': 'F39237',
+        'granite-gray': '646468',
       },
     },
 
