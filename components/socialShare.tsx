65e98b5888d820aeadb6978e0dcfaa4122035aee
Isaiah Taylor <isaiah.p.taylor@gmail.com>
2023-01-02 12:08:26 -0500

    better social

--- components/socialShare.tsx
+++ components/socialShare.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import Image from "next/image";
 import { Facebook } from "./facebook";
 import { Twitter } from "./twitter";
 import { LinkedIn } from "./linkedin";
 
 export const SocialShare: React.FC = () => {
+  const [url, setUrl] = React.useState("");
+
+  useEffect(() => {
+    setUrl(window.location.href);
+  }, []);
+
   return (
     <div className="flex gap-[8px] h-[22px]">
       <a
-        href={`https://twitter.com/intent/tweet?url=${window.location.href}`}
+        href={`https://twitter.com/intent/tweet?url=${url}`}
         target="_blank"
         rel="noreferrer"
         className="flex cursor-pointer"
@@ -16,7 +22,7 @@ export const SocialShare: React.FC = () => {
         <Twitter></Twitter>
       </a>
       <a
-        href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`}
+        href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
         target="_blank"
         rel="noreferrer"
         className="flex cursor-pointer"
@@ -24,7 +30,7 @@ export const SocialShare: React.FC = () => {
         <Facebook></Facebook>
       </a>
       <a
-        href={`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}`}
+        href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
         target="_blank"
         rel="noreferrer"
         className="flex cursor-pointer"
