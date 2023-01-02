beef64b02c7503a455c343adf53fe7cf9eec242e
Isaiah Taylor <isaiah.p.taylor@gmail.com>
2023-01-02 11:25:32 -0500

    upgrade next

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         pathname: '/images/**',
-      }
+      },
     ],
   },
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,44 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useState, useEffect } from 'react';
-import { ThemeContext } from '../contexts/themeContext';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import { ThemeContext } from "../contexts/themeContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
+  const [colorMode, setColorMode] = useState<"light" | "dark">("light");
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       if (window.matchMedia) {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-          setColorMode('dark');
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          setColorMode("dark");
         } else {
-          setColorMode('light');
+          setColorMode("light");
         }
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-          setColorMode(e.matches ? 'dark' : 'light');
-        });
+        window
+          .matchMedia("(prefers-color-scheme: dark)")
+          .addEventListener("change", (e) => {
+            setColorMode(e.matches ? "dark" : "light");
+          });
       }
     }
   }, []);
 
   return (
-    <ThemeContext.Provider value={{
-      colorMode,
-      toggleTheme: () => {
-        setColorMode(colorMode === 'light' ? 'dark' : 'light');
-      }
-    }}>
+    <ThemeContext.Provider
+      value={{
+        colorMode,
+        toggleTheme: () => {
+          setColorMode(colorMode === "light" ? "dark" : "light");
+        },
+      }}
+    >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
