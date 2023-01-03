c0175f524204980225e3f97cc193655fd98e1f6b
Isaiah Taylor <isaiah.p.taylor@gmail.com>
2023-01-03 11:21:42 -0500

    remove useless theme context

--- pages/highlights.tsx
+++ pages/highlights.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { RightSide } from "../components/rightSide";
-import { ThemeContext } from "../contexts/themeContext";
 
 const Highlights: React.FC = () => {
   return (
-    <ThemeContext.Consumer>
-      {({ colorMode }) => (
-        <div className="flex flex-col lg:flex-row">
-          <RightSide />
-          <div className="lg:h-screen pt-[200px] lg:pt-0 font-display text-[20px] flex lg:pl-[200px] justify-center items-center align-middle">
-            {"Nothing here yet, but I'm working on it!"}
-          </div>
-        </div>
-      )}
-    </ThemeContext.Consumer>
+    <div className="flex flex-col lg:flex-row">
+      <RightSide />
+      <div className="lg:h-screen pt-[200px] lg:pt-0 font-display text-[20px] flex lg:pl-[200px] justify-center items-center align-middle">
+        {"Nothing here yet, but I'm working on it!"}
+      </div>
+    </div>
   );
 };
 
